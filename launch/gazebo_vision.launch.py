1f82e94c8627f68e45f0578ffe705d24e55410eb
aidan <aidan.young@haultrax.com>
2026-06-25 01:45:08 +1000

    split launch files properly

---
+++ launch/gazebo_vision.launch.py
@@ -0,0 +1,40 @@
+# Launches everything needed to create the /vision/line_points topics which are pointclouds of the racetrack lines
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+
+def generate_launch_description():
+    package_share = get_package_share_directory('robot_vision')
+    demonstrate_track_outlines_path = os.path.join(package_share, 'launch', 'demonstrate_track_outlines.launch.py')
+    rviz_config_path = os.path.join(package_share, 'config', 'rviz_config.rviz')
+
+    hsv_filter_for_IPM = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('robot_vision'),
+                'launch',
+                'hsv_filter_gazebo.launch.py'
+            )
+        )
+    )
+
+    demonstrate_track_outlines = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(demonstrate_track_outlines_path)
+    )
+    
+    mid_point_draw = Node(
+        package='robot_vision',
+        executable='midpoint_drawer_node',
+        output='screen',
+    )
+
+
+    launch_description = LaunchDescription()
+    launch_description.add_action(hsv_filter_for_IPM)
+    launch_description.add_action(demonstrate_track_outlines)
+    launch_description.add_action(mid_point_draw)
+    return launch_description
